176dce4efdfe39ab1791ca9c65567704090836a5
josephattias <josephattias@outlook.com>
2023-11-09 14:58:37 -0500

    Added compilation feature. and debug msgs

--- Simulink/ardupilot_controller_v1/generate_and_integrate.m
+++ Simulink/ardupilot_controller_v1/generate_and_integrate.m
@@ -1,9 +1,11 @@
 clear
-
+clc
+fprintf("****** GENERATE | INTEGRATE | BUILDERATE ******\n\n")
 %% The project files should reflect the project folder name 
 projectdir = pwd;
 projectdirslashes = regexp(projectdir,"/","end");
 projectname = projectdir(projectdirslashes(end)+1:end);
+fprintf("*** Custom controller name: "+projectname+" ***\n")
 
 %% Generate code with supporting files
 run(projectname+"_variables")
@@ -15,11 +17,11 @@ end
 currentDateTime = string(datetime("now","Format","MMM-dd-uuuu_HHmmss"));
 
 try movefile("../../libraries/AC_Simulink","../libraries/AC_Simulink_"+currentDateTime);
-    fprintf("Past AC_Simulink archived at: "+ ... 
+    fprintf("\n*** Past AC_Simulink archived at: "+ ... 
         projectdir(projectdirslashes(end-1)+1:projectdirslashes(end))+ ... 
-        "libraries/AC_Simulink_"+currentDateTime+"\n")
+        "libraries/AC_Simulink_"+currentDateTime+" ***\n")
 catch 
-    fprintf("AC_Simulink does not yet exsist. Skipping archive.\n")
+    fprintf("\n*** AC_Simulink does not yet exsist. Skipping archive. ***\n")
 end
 
 %% Unzip the code package and move files to library folder
@@ -28,3 +30,11 @@ copyfile(projectname+"_code/"+projectname+"/"+projectname+"_model_ert_rtw","../.
 
 %% Modify code files with standard name 
 % TODO
+
+%% compile 
+fprintf("\n*** Building source code ***\n")
+cd('../..');
+system('./waf configure');
+% system('./waf copter')
+cd(projectdir);
+%% upload command
