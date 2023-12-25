66a63f78f0d765f66e37d4a0f080255395b344a0
josephattias <josephattias@outlook.com>
2023-12-25 13:03:44 -0500

    upload script updates

--- Simulink/wrapper_template/generate_and_integrate.m
+++ Simulink/wrapper_template/generate_and_integrate.m
@@ -1,6 +1,7 @@
 clear
 clc
-fprintf("****** GENERATE | INTEGRATE ******\n\n")
+buildVars = struct();
+fprintf("****** GENERATE | INTEGRATE | BUILDERATE ******\n\n")
 
 % WELCOME! 
 % The following script is designed to automate the process between
@@ -14,80 +15,82 @@ fprintf("****** GENERATE | INTEGRATE ******\n\n")
 %    4. Changing the name of the header file of the CustomControl_Simulink
 %       wrapper to that of the generated code. 
 %    5. Compiling the source code into a flashable binary. 
+%    6. Flashing the binary onto the flight controller. 
 % 
 % Created by: Joseph Attias (josephattias@campus.technion.ac.il)
 % Created on: Nov 9, 2023
 
 %% The project files should reflect the project folder name 
-projectdir = pwd;
-projectdirslashes = regexp(projectdir,"/","end");
-projectname = projectdir(projectdirslashes(end)+1:end);
-fprintf("*** Custom controller name: "+projectname+" ***\n")
+buildVars.projectdir = pwd;
+buildVars.projectdirslashes = regexp(buildVars.projectdir,"/","end");
+buildVars.projectname = buildVars.projectdir(buildVars.projectdirslashes(end)+1:end);
+fprintf("*** Custom controller name: "+buildVars.projectname+" ***\n")
 
 %% Generate code with supporting files
 SCHED_LOOP_RATE = 50; % https://ardupilot.org/dev/docs/copter-adding-custom-controller.html#preparing-your-simulink-model-for-code-generation 
 
-run(projectname+"_script")
-try slbuild(projectname+"_model")
-catch error("Code did not generate properly")
+run(buildVars.projectname+"_script")
+try slbuild(buildVars.projectname+"_model")
+catch 
+    error("Code did not generate properly")
 end
 
 %% Archive the past library folder 
-currentDateTime = string(datetime("now","Format","MMM-dd-uuuu_HHmmss"));
-% TODO Check to see if the code is any different. Something like a checksum hash comparison. 
-% Or use the response string from code-generation: 
-    % "0 of 1 models built (1 models already up to date)"
+buildVars.currentDateTime = string(datetime("now","Format","MMM-dd-uuuu_HHmmss"));
+% TODO Check to see if the code is any different. Something like a checksum
+% hash comparison. 
 
-try movefile("../../libraries/AC_Simulink","../libraries/AC_Simulink_"+currentDateTime);
+try movefile("../../libraries/AC_Simulink","../libraries/AC_Simulink_"+buildVars.currentDateTime);
     fprintf("\n*** Past AC_Simulink archived at: "+ ... 
-        projectdir(projectdirslashes(end-1)+1:projectdirslashes(end))+ ... 
-        "libraries/AC_Simulink_"+currentDateTime+" ***\n")
+        buildVars.projectdir(buildVars.projectdirslashes(end-1)+1:buildVars.projectdirslashes(end))+ ... 
+        "libraries/AC_Simulink_"+buildVars.currentDateTime+" ***\n")
 catch 
     fprintf("\n*** AC_Simulink does not yet exsist. Skipping archive. ***\n")
 end
 
 %% Move files to library folder
-unzip(projectname+"_model.zip",projectname+"_code")
-copyfile(projectname+"_code/"+projectname+"/"+projectname+"_model_ert_rtw","../../libraries/AC_Simulink/");
+unzip(buildVars.projectname+"_model.zip",buildVars.projectname+"_code")
+copyfile(buildVars.projectname+"_code/"+buildVars.projectname+"/"+buildVars.projectname+"_model_ert_rtw","../../libraries/AC_Simulink/");
 fprintf("*** Generated code is located here: "+ ...
-    "../../libraries/AC_Simulink/" + projectname +".h"+...
+    "../../libraries/AC_Simulink/" + buildVars.projectname +".h"+...
     " ***\n")
 
 %% Modify code files with standard name 
 % Read the content of the header file
-file_path = "../../libraries/AC_CustomControl/AC_CustomControl_Simulink.h";
-file_content = fileread(file_path);
+buildVars.file_path = "../../libraries/AC_CustomControl/AC_CustomControl_Simulink.h";
+buildVars.file_content = fileread(buildVars.file_path);
 
 % Find the name of the old model to be replaced
-old_model_regexp = "\w*(?=_model\.h>)";
-existing_string_start = regexp(file_content,old_model_regexp,"start");
-existing_string_end   = regexp(file_content,old_model_regexp,"end");
-old_model_name = file_content(existing_string_start:existing_string_end);
+buildVars.old_model_regexp = "\w*(?=_model\.h>)";
+buildVars.existing_string_start = regexp(buildVars.file_content,buildVars.old_model_regexp,"start");
+buildVars.existing_string_end   = regexp(buildVars.file_content,buildVars.old_model_regexp,"end");
+buildVars.old_model_name = buildVars.file_content(buildVars.existing_string_start:buildVars.existing_string_end);
 
-if ~strcmp(old_model_name,projectname)
+if ~strcmp(buildVars.old_model_name,buildVars.projectname)
     fprintf("\n*** New project name: cleaning build system. Expect longer build ***\n");
     system("../../waf clean");
 end
 
-new_model_name = projectname;
+buildVars.new_model_name = buildVars.projectname;
 
 % Perform the replacement
-modified_content = strrep(file_content, old_model_name, new_model_name);
+buildVars.modified_content = strrep(buildVars.file_content, buildVars.old_model_name, buildVars.new_model_name);
 
 % Write the modified content back to the file
-fid = fopen(file_path, 'w');
-fprintf(fid, '%s', modified_content);
-fclose(fid);
+buildVars.fid = fopen(buildVars.file_path, 'w');
+fprintf(buildVars.fid, '%s', buildVars.modified_content);
+fclose(buildVars.fid);
 
 
 %% compile 
 cd('../..');
 fprintf("\n*** Configuring source code for flight controller ***\n")
-system('./waf configure');
+system('./waf configure --board BETAFPV-F405');
 fprintf("\n*** Building ArduCopter source code ***\n")
-system('./waf copter');
-cd(projectdir);
+system('./waf copter -j8');
 
 %% upload command
-% TODO 
+fprintf("\n*** Uploading to flight controller ***\n")
+system('./waf copter --upload')
 
+cd(buildVars.projectdir);
