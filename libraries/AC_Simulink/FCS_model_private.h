729c8551603741e610bc4e991263d36a30ccfe8e
Joseph Attias <josephattias@outlook.com>
2024-02-28 08:09:27 -0500

    fixed the gitignore and FCS reference

---
+++ libraries/AC_Simulink/FCS_model_private.h
@@ -0,0 +1,32 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: FCS_model_private.h
+//
+// Code generated for Simulink model 'FCS_model'.
+//
+// Model version                  : 1.12
+// Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+// C/C++ source code generated on : Tue Feb 27 17:32:46 2024
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: ARM Compatible->ARM Cortex-M
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_FCS_model_private_h_
+#define RTW_HEADER_FCS_model_private_h_
+#include "rtwtypes.h"
+#include "FCS_model_types.h"
+
+extern real32_T rt_atan2f_snf(real32_T u0, real32_T u1);
+
+#endif                                 // RTW_HEADER_FCS_model_private_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
